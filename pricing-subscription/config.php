6cbdb9b761a67f301a5d72e7530120b6068b731d
Naman Kumar <59838992+namankr85100@users.noreply.github.com>
2020-10-04 09:44:57 +0530

    Add files via upload

---
+++ pricing-subscription/config.php
@@ -0,0 +1,8 @@
+<?php
+$SETTINGS["mysql_user"]='root';
+$SETTINGS["mysql_pass"]='';
+$SETTINGS["hostname"]='localhost';
+$SETTINGS["mysql_database"]='demo';
+$SETTINGS["data_table"]='registrations';
+$SETTINGS["paypal_address"]='email@domain.com';
+?>
